0083f64afd546abf5a4e1cab729cb79b2ca68b92
SergejYermakovich <sergu9848@gmail.com>
2024-08-06 17:07:38 +0300

    init project

---
+++ src/main/java/com/app/scoot/ScootApplication.java
@@ -0,0 +1,13 @@
+package com.app.scoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/scoot/ScootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.scoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
